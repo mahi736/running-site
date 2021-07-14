a7b099850c8d682bc710ecf350f7c27612b58767
mahi736 <mdmahin5531@gmail.com>
2021-07-14 10:26:34 +0600

    Alas feature not published

--- understand.js
+++ understand.js
@@ -17,3 +17,23 @@ undButton.addEventListener('click', (e)=>{
         e.target.parentNode.parentNode.style.display = 'none';
     }, 700);  
 })
+
+if(localStorage.getItem('busy') == null){
+    localStorage.setItem('busy', false);
+    var status = false;
+} else {
+    var status = localStorage.getItem('busy');
+}
+var busy = document.getElementById('busy');
+var undButton2 = document.getElementById('undButton2');
+if(status == 'true'){
+    busy.style.display = 'none';
+}
+undButton2.addEventListener('click', (e)=>{
+    localStorage.removeItem('busy');
+    localStorage.setItem('busy', true);
+    e.target.parentNode.parentNode.parentNode.parentNode.classList.add('dowAnimation');
+    setTimeout(() => {
+        e.target.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+    }, 700);  
+})
